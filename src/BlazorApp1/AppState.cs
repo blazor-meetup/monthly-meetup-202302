291d00951e9d45fe9e70b9b6250b81b6b4bae401
김 진석(Kim, JinSeok) <jinseok.kim@khsystems.co.kr>
2023-03-04 22:08:58 +0900

    2월 밋업 사용 샘플 코드 추가 .gitignore 추가

---
+++ src/BlazorApp1/AppState.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorApp1
+{
+    public class AppState
+    {
+        public int count = 0;
+        public event Action? OnChange;
+
+        public void increaseCount()
+        {
+            count++;
+            NotifyStateChanged();
+        }
+
+        public void decreaseCount()
+        {
+            count--;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
